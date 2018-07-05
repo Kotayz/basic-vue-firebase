99df16b281739b392449a664613b8dc1b6106c20
elson.almeida <elson.almeida@tivit.com.br>
2018-07-05 15:25:35 -0300

    First Commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import { store } from './store'
+// import firebase from 'firebase'
+
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
+
+// firebase.initializeApp({
+//   apiKey: 'YOUR_API_KEY',
+//   authDomain: 'YOUR_AUTH_DOMAIN',
+//   databaseURL: 'YOUR_DATABASE_URL',
+//   projectId: 'YOUR_PROJECT_ID'
+// })
